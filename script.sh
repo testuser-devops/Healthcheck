0de6c4e525eab9bd41c85f0ce4922c27589ec62b
testuser-devops <126338398+testuser-devops@users.noreply.github.com>
2023-02-26 15:23:14 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 ls
 systemctl --type=service >>usagereport
 cat usagereport
+git config --global --add safe.directory '*'
 git config --global user.email "test.tester9676@gmail.com"
 git config --global user.name "testuser-devops"
 git add .
