be47132d249c4917b9f351c7353005447bd00770
testuser-devops <test.tester9676@gmail.com>
2023-02-27 12:28:58 +0530

    new file added

--- serverhealth.ps1
+++ serverhealth.ps1
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 for i in hostlist;
 do
   Get-ServerHealth -Identity $i >>serverhealth
@@ -7,6 +7,7 @@ do
   CPUSAGE=$(ssh $i top -b -n 1 -d1 | grep "Cpu" | awl '{print $2}' | awk -F. '{print $1}')
   MEMUSAGE=$(ssh $i free | grep Mem | awk '{print $3/$2 * 100.0}')
   DISKUSAGE=$(ssh $i df -P | column -t | awk '{print $5}' | tail -n 1 | sed 's/%//g')
+  Get-Process | Where-Object {$_.CPU -gt 1} >>processlist
   echo 'HOSTNAME, DATE&TIME, CPUI(%), MEM(%), DISK(%)' >>usagereport
   echo "$HOSTNAME, $DATE, $CPUSAGE, $MEMUSAGE, $DISKUSAGE" >>usagereport
 done
